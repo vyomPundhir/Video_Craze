129fd2ffddaed113b48f9fa9fb9d7d1469ded804
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-07-18 23:40:17 +0530

    Sidebar added.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx:"column", md: "row"}}}>
+      <Box sx={{height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx:0, md: 2}}}>
+        SideBar
+
+        <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#ffffff'}}>
+          Copyright 2023 YT Media
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
