6e517fc862cda2da5684ecbc53e0cf97236f853b
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-07-21 22:13:46 +0530

    VideoCard file updated.

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,12 +7,12 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
 
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180}}
+          sx={{ width: 358, height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
@@ -21,9 +21,15 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
           </Typography>
         </Link>
+
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+          </Typography>
+        </Link>
       </CardContent>
     </Card>
   )
 }
-
-export default VideoCard
+export default VideoCard;
