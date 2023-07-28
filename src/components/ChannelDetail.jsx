72e01dcd8d181464eeb202cb5092059c45b4c6d0
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-07-28 23:56:10 +0530

    Channel detail file modified.

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -7,21 +7,36 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null)
-  const [ videos, setVideos ] = useState(second)
+  const [ videos, setVideos ] = useState([])
   const { id } = useParams();
 
-  console.log(channelDetail)
+  console.log(channelDetail, videos)
 
   useEffect(() => {
     fetchFromAPI(`channels?part=snippet&id=${id}`)
       .then((data) => setChannelDetail(data?.items[0]));
 
-      fetchFromAPI(`search?channelId=${id}&part=snippet&order=data`)
-      .then((data) => setChannelDetail(data?.items[0]));
+      fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
   }, [id])
   
   return (
-    <div>{id}</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0, 212, 255, 1) 100%)',
+          zIndex: 10,
+          height: '300px'
+        }}
+        />
+          <ChannelCard channelDetail={channelDetail}
+          marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '100px' }}} />
+          <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
